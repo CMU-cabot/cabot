523828f897413db7b87ae5907aa235c30830cdcd
Daisuke Sato <daisukes@cmu.edu>
2021-01-14 14:43:14 -0500

    update docker-compose version

--- tools/install-docker.sh
+++ tools/install-docker.sh
@@ -53,6 +53,6 @@ sudo gpasswd -a $USER docker
 
 
 # install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.25.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
