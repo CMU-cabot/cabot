14ab00869db4b7310480ba72027f11e368e9647a
Daisuke Sato <daisukes@cmu.edu>
2021-01-14 14:42:51 -0500

    include odrive udev rules

--- tools/setup-usb.sh
+++ tools/setup-usb.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo cp config/10-cabot.rules /etc/udev/rules.d/10-cabot.rules 
+pwd=`pwd`
+scriptdir=`dirname $0`
+cd $scriptdir
+scriptdir=`pwd`
+
+sudo cp $scriptdir/config/10-cabot.rules /etc/udev/rules.d/10-cabot.rules 
