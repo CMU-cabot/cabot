838b6c7b5e6e59ddb736ff665f484b408e54387f
tisihara <tisihara@jp.ibm.com>
2021-12-10 23:34:36 +0900

    Fix build erros of darknet, realsense-ros on Jetson JetPack4.6
    DCO 1.1 Signed-off-by: Tatsuya Ishihara <tisihara@jp.ibm.com>

--- tools/setup-jetson.sh
+++ tools/setup-jetson.sh
@@ -2,13 +2,15 @@
 
 
 sudo apt update
-sudo apt remove thunderbird libreoffice-* -y
+sudo apt purge thunderbird libreoffice-* chromium-browser fonts-noto-cjk -y
 
 sudo cp config/daemon.json /etc/docker/
 sudo pkill -SIGHUP dockerd
 sudo gpasswd -a $USER docker
 
 # install docker-compose
+sudo apt update
+sudo apt install --no-install-recommends -y curl
 sudo curl -L https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
@@ -16,10 +18,11 @@ sudo apt update
 sudo apt install --no-install-recommends -y \
 	nvidia-docker2 \
 	nvidia-cudnn8 libcublas-dev libcurand-dev-10-2 \
+	cuda-nvcc-10-2  nvidia-container-csv-cudnn \
 	python3-pip python3-setuptools
 sudo pip3 install -U vcstool
 
-sudo rm -rf/var/lib/apt/lists/*
+sudo rm -rf /var/lib/apt/lists/*
 sudo apt autoremove -y
 sudo apt clean
-
+rm -rf ~/.cache
