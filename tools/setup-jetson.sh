1f2988faddfcba09c213c5ad03adb2c49495eb64
Daisuke Sato <daisukes@cmu.edu>
2021-12-14 12:18:21 -0500

    update jetson setup script for opencv dnn

--- tools/setup-jetson.sh
+++ tools/setup-jetson.sh
@@ -11,15 +11,21 @@ sudo gpasswd -a $USER docker
 # install docker-compose
 sudo apt update
 sudo apt install --no-install-recommends -y curl
-sudo curl -L https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo apt update
 sudo apt install --no-install-recommends -y \
 	nvidia-docker2 \
-	nvidia-cudnn8 libcublas-dev libcurand-dev-10-2 \
-	cuda-nvcc-10-2  nvidia-container-csv-cudnn \
-	python3-pip python3-setuptools
+	nvidia-cudnn8 \
+	libcublas-dev \
+	libcurand-dev-10-2 \
+	libnpp-dev-10-2 \
+	libcufft-dev-10-2 \
+	cuda-nvcc-10-2  \
+	nvidia-container-csv-cudnn \
+	python3-pip \
+	python3-setuptools
 sudo pip3 install -U vcstool
 
 sudo rm -rf /var/lib/apt/lists/*
