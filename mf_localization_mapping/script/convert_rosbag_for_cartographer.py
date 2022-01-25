70db3bd938f5f33261235211d86e0da2a5c536de
Masayuki Murata <muratams@jp.ibm.com>
2022-01-25 17:26:25 +0900

    update the tool to fix imu messages in a bag file
    Signed-off-by: Masayuki Murata <muratams@jp.ibm.com>

--- mf_localization_mapping/script/convert_rosbag_for_cartographer.py
+++ mf_localization_mapping/script/convert_rosbag_for_cartographer.py
@@ -30,12 +30,18 @@ import rosbag
 
 def main():
     parser = argparse.ArgumentParser("convert imu frame name to 'imu'")
-    parser.add_argument("-i","--input", required=True)
-    parser.add_argument("-o","--output", required=True)
+    parser.add_argument("-i","--input", required=True, help="input bag file")
+    parser.add_argument("-o","--output", required=True, help="output bag file")
+    parser.add_argument("--imu_topic", default="/imu/data")
+    parser.add_argument("--imu_frame", default="imu")
+    parser.add_argument("--fix_imu_timestamp", default=False, action="store_true")
     args = parser.parse_args()
 
     input_bag = args.input
     output_bag = args.output
+    imu_topic = args.imu_topic
+    imu_frame = args.imu_frame
+    fix_imu_timestamp = args.fix_imu_timestamp
 
     if input_bag == output_bag:
         raise RuntimeError("input == output")
@@ -46,11 +52,29 @@ def main():
     with rosbag.Bag(input_bag) as bag:
         for topic, msg, t in bag.read_messages():
 
-            if topic == "/imu/data":
-                msg.header.frame_id = "imu"
+            if topic == imu_topic:
+                # replace imu_frame if necessary
+                if imu_frame != "":
+                    msg.header.frame_id = imu_frame
 
-            if topic in ["/velodyne_points", "/imu/data", "/beacons", "/wireless/beacons", "/wireless/wifi"]:
+                # imu value check
+                ax = msg.linear_acceleration.x
+                ay = msg.linear_acceleration.y
+                az = msg.linear_acceleration.z
+                amp = (ax**2 + ay**2 + az**2)**0.5
+                if amp == 0.0:
+                    print("skipped invalid imu value. (linear_accleration==0)")
+                    continue
+
+            if topic in ["/velodyne_points","/beacons", "/wireless/beacons", "/wireless/wifi"]:
                 outbag.write(topic, msg, t)
+            elif topic ==  imu_topic:
+                # replace imu message timestamps with header timestamps if necessary
+                if fix_imu_timestamp:
+                    outbag.write(topic, msg, msg.header.stamp)
+                else:
+                    outbag.write(topic, msg, t)
+
     outbag.close()
 
 if __name__ == "__main__":
