2a27c90459779c6dbddd3dc7dbc8f2da34df72f0
Daisuke Sato <daisukes@cmu.edu>
2022-03-02 14:33:06 -0500

    add uninstall script for the service

---
+++ tools/uninstall-service.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Copyright (c) 2022  Carnegie Mellon University
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+if [ $(id -u) -eq 0 ]; then
+   echo "please do not run as root: $0"
+   exit
+fi
+
+pwd=`pwd`
+scriptdir=`dirname $0`
+cd $scriptdir
+scriptdir=`pwd`
+
+cd $scriptdir/../
+projectdir=`pwd`
+
+## uninstall cabot.service
+INSTALL_DIR=$HOME/.config/systemd/user
+systemctl --user disable cabot
+rm $INSTALL_DIR/cabot.service
+
+## uninstall nvidia-smi sudo pliviledge
+USERNAME=$(id -un)
+sudo rm /etc/sudoers.d/$USERNAME
+
+## remove symlink
+sudo rm /opt/cabot
