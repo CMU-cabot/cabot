bb0d4227fa6b50d032b98ef93a45ff5a12230baa
Daisuke Sato <daisukes@cmu.edu>
2022-11-09 14:58:34 -0500

    use cabot-arduino-serial

--- cabot/python/cabot_serial.py
+++ cabot/python/cabot_serial.py
@@ -20,23 +20,25 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import logging
+import multiprocessing
+import sys
+import struct
 import termios
 import traceback
+from time import sleep, time
+
+from serial import Serial, SerialException
 
 import rospy
-from rosserial_python import SerialClient, RosSerialServer
-from serial import SerialException, Serial
-from time import sleep, time
-import multiprocessing
+
 from cabot.util import setInterval
 from sensor_msgs.msg import Imu, FluidPressure, Temperature
-from std_msgs.msg import Bool, Int8, Int16, Float32, Float32MultiArray
+from std_msgs.msg import Bool, UInt8, UInt8MultiArray, Int8, Int16, Float32, Float32MultiArray
 from std_srvs.srv import SetBool, SetBoolResponse
 from diagnostic_updater import Updater, DiagnosticTask, HeaderlessTopicDiagnostic, FrequencyStatusParam
 from diagnostic_msgs.msg import DiagnosticStatus
 
-import sys
-import struct
 
 # global variables
 imu_last_topic_time = None
@@ -45,12 +47,14 @@ btn_pubs = []
 NUMBER_OF_BUTTONS = 5
 btn_sub = None
 
-def imu_callback(msg):
+def imu_callback(data):
     global imu_last_topic_time
     ## discard possible corrupted data
     count = 0
+    data2 = [struct.unpack('f', data[i*4:(i+1)*4])[0] for i in range(0, 12)]
+
     for i in range(2, 12):
-        if msg.data[i] == 0:
+        if data2[i] == 0:
             count += 1
     if count > 3:
         return
@@ -61,8 +65,8 @@ def imu_callback(msg):
     imu_msg.orientation_covariance[8] = 0.1
 
     # convert float(32) to int(32)
-    imu_msg.header.stamp.set(struct.unpack('i', struct.pack('f', msg.data[0]))[0]
-                             ,struct.unpack('i', struct.pack('f', msg.data[1]))[0])
+    imu_msg.header.stamp.set(struct.unpack('i', struct.pack('f', data2[0]))[0]
+                             ,struct.unpack('i', struct.pack('f', data2[1]))[0])
     if imu_last_topic_time is not None:
         if imu_last_topic_time > imu_msg.header.stamp:
             rospy.logerr("IMU timestamp is not consistent, drop a message\n"+
@@ -72,16 +76,17 @@ def imu_callback(msg):
 
     imu_msg.header.frame_id = "imu_frame"
     imu_last_topic_time = imu_msg.header.stamp
-    imu_msg.orientation.x = msg.data[2]
-    imu_msg.orientation.y = msg.data[3]
-    imu_msg.orientation.z = msg.data[4]
-    imu_msg.orientation.w = msg.data[5]
-    imu_msg.angular_velocity.x = msg.data[6]
-    imu_msg.angular_velocity.y = msg.data[7]
-    imu_msg.angular_velocity.z = msg.data[8]
-    imu_msg.linear_acceleration.x = msg.data[9]
-    imu_msg.linear_acceleration.y = msg.data[10]
-    imu_msg.linear_acceleration.z = msg.data[11]
+    imu_msg.orientation.x = data2[2]
+    imu_msg.orientation.y = data2[3]
+    imu_msg.orientation.z = data2[4]
+    imu_msg.orientation.w = data2[5]
+    imu_msg.angular_velocity.x = data2[6]
+    imu_msg.angular_velocity.y = data2[7]
+    imu_msg.angular_velocity.z = data2[8]
+    imu_msg.linear_acceleration.x = data2[9]
+    imu_msg.linear_acceleration.y = data2[10]
+    imu_msg.linear_acceleration.z = data2[11]
+
     imu_pub.publish(imu_msg)
 
 
@@ -154,6 +159,98 @@ class CheckConnectionTask(DiagnosticTask):
                 stat.summary(DiagnosticStatus.OK, "working")
 
 
+from cabot.arduino_serial import CaBotArduinoSerialDelegate, CaBotArduinoSerial
+stopped = False
+class ROSDelegate(CaBotArduinoSerialDelegate):
+    def __init__(self):
+        self.owner = None
+
+        self.touch_raw_pub = rospy.Publisher("/touch_raw", Int16, queue_size=10)
+        self.touch_pub = rospy.Publisher("/touch", Int16, queue_size=10)
+        self.button_pub = rospy.Publisher("/pushed", Int8, queue_size=10)
+        self.imu_last_topic_time = None
+        self.imu_pub = rospy.Publisher("/imu", Imu, queue_size=10)
+        self.calibration_pub = rospy.Publisher("/calibration", UInt8MultiArray, queue_size=10)
+        self.pressure_pub = rospy.Publisher("/pressure", FluidPressure, queue_size=10)
+        self.temperature_pub = rospy.Publisher("/temperature", Temperature, queue_size=10)
+
+        self.vib1_sub = rospy.Subscriber("/vibrator1", UInt8, self.vib_callback(0x20))
+        self.vib2_sub = rospy.Subscriber("/vibrator2", UInt8, self.vib_callback(0x21))
+        self.vib3_sub = rospy.Subscriber("/vibrator3", UInt8, self.vib_callback(0x22))
+        self.vib4_sub = rospy.Subscriber("/vibrator4", UInt8, self.vib_callback(0x23))
+
+    def vib_callback(self, cmd):
+        def callback(msg):
+            data = bytearray()
+            data.append(msg.data)
+            self.owner.send_command(cmd, data, 1)
+        return callback
+
+    def system_time(self):
+        return rospy.Time.now().to_sec()
+
+    def stopped(self):
+        global stopped
+        stopped = True
+
+    def log(self, level, text):
+        if level == logging.INFO:
+            rospy.loginfo(text)
+        if level == logging.WARN:
+            rospy.logwarn(text)
+        if level == logging.ERROR:
+            rospy.logerr(text)
+
+    def log_throttle(self, level, interval, text):
+        if level == logging.INFO:
+            rospy.loginfo_throttle(interval, text)
+        if level == logging.WARN:
+            rospy.logwarn_throttle(interval, text)
+        if level == logging.ERROR:
+            rospy.logerr_throttle(interval, text)
+
+    def get_param(self, name, callback):
+        val = rospy.get_param(name, None)
+        if val:
+            callback(val)
+
+    def publish(self, cmd, data):
+        #rospy.loginfo("%x: %d", cmd, int.from_bytes(data, "little"))
+        # rospy.loginfo("%x: %s", cmd, str(data));
+        #self.log_throttle(logging.INFO, 1, "got data %x"%(cmd))
+        if cmd == 0x10:  # touch
+            msg = Int16()
+            msg.data = int.from_bytes(data, 'little')
+            self.touch_pub.publish(msg)
+        if cmd == 0x11:  # touch_raw
+            msg = Int16()
+            msg.data = int.from_bytes(data, 'little')
+            self.touch_raw_pub.publish(msg)
+        if cmd == 0x12:  # buttons
+            msg = Int8()
+            msg.data = int.from_bytes(data, 'little')
+            self.button_pub.publish(msg)
+        if cmd == 0x13:  # imu
+            imu_callback(data)
+        if cmd == 0x14:  # calibration
+            msg = UInt8MultiArray()
+            msg.data = data
+            self.calibration_pub.publish(msg)
+        if cmd == 0x15:  # pressure
+            msg = FluidPressure()
+            msg.fluid_pressure = struct.unpack('f', data)[0]
+            msg.variance = 0;
+            msg.header.stamp = rospy.Time.now();
+            msg.header.frame_id = "bmp_frame";
+            self.pressure_pub.publish(msg)
+        if cmd == 0x16:  # temperature
+            msg = Temperature()
+            msg.temperature = struct.unpack('f', data)[0]
+            msg.variance = 0;
+            msg.header.stamp = rospy.Time.now();
+            msg.header.frame_id = "bmp_frame";
+            self.temperature_pub.publish(msg)
+
 if __name__=="__main__":
     rospy.init_node("cabot_serial_node")
     rospy.loginfo("CABOT ROS Serial Python Node")
@@ -174,14 +271,14 @@ if __name__=="__main__":
 
     ## Diagnostic Updater
     updater = Updater()
-    TopicCheckTask(updater, "IMU", "imu_raw", Float32MultiArray, 98, imu_callback)
+    TopicCheckTask(updater, "IMU", "imu", Imu, 100)
     TopicCheckTask(updater, "Touch Sensor", "touch", Int16, 50, touch_callback)
     for i in range(1, NUMBER_OF_BUTTONS+1):
         TopicCheckTask(updater, "Push Button %d"%(i), "pushed_%d"%(i), Bool, 50)
     TopicCheckTask(updater, "Pressure", "pressure", FluidPressure, 2)
     TopicCheckTask(updater, "Temperature", "temperature", Temperature, 2)
-    rospy.Timer(rospy.Duration(1), lambda e: updater.update())
-    updater.add(CheckConnectionTask("Rosserial Connection"))
+    rospy.Timer(rospy.Duration(0.5), lambda e: updater.update())
+    updater.add(CheckConnectionTask("Serial Connection"))
 
     ## add the following line into /etc/udev/rules.d/10-local.rules
     ## ACTION=="add", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", SYMLINK+="ttyCABOT"
@@ -198,6 +295,7 @@ if __name__=="__main__":
     sleep_time=3
     
     error_msg = None
+    delegate = ROSDelegate()
     while not rospy.is_shutdown():
         try:
             client = None
@@ -216,10 +314,15 @@ if __name__=="__main__":
                     error_msg = str(e)
                     rospy.logerr("%s", e)
                     sleep(3)
-            client = SerialClient(port, baud)
+
+            client = CaBotArduinoSerial(port, baud)
+            delegate.owner = client
+            client.delegate = delegate
             updater.setHardwareID(port_name)
             topic_alive = None
-            client.run()
+            client.start()
+            while client.is_alive:
+                rospy.rostime.wallsleep(0.1)
         except KeyboardInterrupt as e:
             rospy.loginfo("KeyboardInterrupt")
             rospy.signal_shutdown("user interrupted")
