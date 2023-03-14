620b5ea922fa23794023399fac235b3d7b169e0d
Daisuke Sato <daisukes@cmu.edu>
2023-03-15 07:31:48 +0900

    velocity obstacle logic fix
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- cabot_navigation2/plugins/velocity_obstacle.cpp
+++ cabot_navigation2/plugins/velocity_obstacle.cpp
@@ -76,10 +76,7 @@ class VelocityObstacleCritic : public dwb_critics::BaseObstacleCritic {
     if (vel < low_speed_threshold_) {
       return 0;
     }
-    if (pose_score < std::pow(1-vel, factor_) * cost_threshold_) {
-      return 0;
-    }
-    return pose_score;
+    return pose_score * std::pow(1-vel, factor_);
   }
 
  private:
