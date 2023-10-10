9a83ddcea801bda419ca65d2071ca6b675e05df9
Daisuke Sato <daisukes@cmu.edu>
2023-10-10 12:23:20 -0400

    (#131) remove dependencies of required linux commands (curl/jq) to setup MapService server
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- docker/server/server-data.sh
+++ docker/server/server-data.sh
@@ -91,7 +91,7 @@ scriptdir=`pwd`
 temp_dir=$scriptdir/../.tmp
 mkdir -p $temp_dir
 
-HOST=http://localhost:9090/map
+HOST=http://map_server:9090/map
 API_KEY=local-server-editor-api-key
 
 cd $temp_dir
