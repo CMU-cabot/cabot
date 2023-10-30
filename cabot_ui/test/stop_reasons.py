7c6caa180a9a53293d5a5fe916a597a032b0f49c
Daisuke Sato <daisukes@cmu.edu>
2023-10-29 22:15:29 -0400

    bugfix stop reasoner py
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- cabot_ui/test/stop_reasons.py
+++ cabot_ui/test/stop_reasons.py
@@ -2,12 +2,14 @@
 
 import logging
 import os
+from optparse import OptionParser
 import sys
+import rclpy
 from tf_transformations import quaternion_multiply, euler_from_quaternion
 
 from rclpy.serialization import deserialize_message
 from rosidl_runtime_py.utilities import get_message
-import rosbag2_py
+from cabot_common.rosbag2 import BagReader
 
 import cabot_ui.geoutil
 from cabot_ui.stop_reasoner import StopReasoner, StopReason, StopReasonFilter
@@ -16,66 +18,73 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG if 'DEBUG' in os.environ else logging.INFO)
 
-def get_rosbag_options(path, serialization_format='cdr'):
-    storage_options = rosbag2_py.StorageOptions(uri=path)
+parser = OptionParser(usage="""
+print/plot topics
+Example
+{0} -f <bag file>                       # run stop reasoner
+(support only one topic)
+""".format(sys.argv[0]))
 
-    converter_options = rosbag2_py.ConverterOptions(
-        input_serialization_format=serialization_format,
-        output_serialization_format=serialization_format)
+parser.add_option('-f', '--file', type=str, help='bag file to print')
+parser.add_option('-s', '--start', type=float, help='start time from the begining', default=0.0)
+parser.add_option('-d', '--duration', type=float, help='duration from the start time', default=99999999999999)
 
-    return storage_options, converter_options
+(options, args) = parser.parse_args()
 
-def read_from_bag(bagfile, callback=None):
-    storage_options, converter_options = get_rosbag_options(bagfile)
-    reader = rosbag2_py.SequentialReader()
-    reader.open(storage_options, converter_options)
-    
-    topic_types = reader.get_all_topics_and_types()
-    type_map = {topic_types[i].name: topic_types[i].type for i in range(len(topic_types))}
-    
-    #tf_transformer = tf_bag.BagTfTransformer(bag)
-    tf_transformer = None
-    reasoner = StopReasoner(tf_transformer)
+if not options.file:
+    parser.print_help()
+    sys.exit(0)
 
-    prev_code = None
-    prev_duration = 0
+reader = BagReader(options.file)
 
-    ODOM_TOPIC="/cabot/odom_raw"
-    EVENT_TOPIC="/cabot/event"
-    CMD_VEL_TOPIC="/cmd_vel"
-    PEOPLE_SPEED_TOPIC="/cabot/people_speed"
-    TF_SPEED_TOPIC="/cabot/tf_speed"
-    TOUCH_SPEED_TOPIC="/cabot/touch_speed_switched"
-    NAVIGATE_TO_POSE_GOAL_TOPIC="/navigate_to_pose/goal"
-    NAVIGATE_TO_POSE_RESULT_TOPIC="/navigate_to_pose/result"
-    LOCAL_PREFIX="/local"
-    REPLAN_REASON_TOPIC="/replan_reason"
-    CURRENT_FRAME_TOPIC="/current_frame"
+topic_types = reader.get_all_topics_and_types()
+type_map = {topic_types[i].name: topic_types[i].type for i in range(len(topic_types))}
 
-    all_topics = [
-        ODOM_TOPIC,
-        EVENT_TOPIC,
-        CMD_VEL_TOPIC,
-        PEOPLE_SPEED_TOPIC,
-        TF_SPEED_TOPIC,
-        TOUCH_SPEED_TOPIC,
-        NAVIGATE_TO_POSE_GOAL_TOPIC,
-        NAVIGATE_TO_POSE_RESULT_TOPIC,
-        LOCAL_PREFIX+NAVIGATE_TO_POSE_GOAL_TOPIC,
-        LOCAL_PREFIX+NAVIGATE_TO_POSE_RESULT_TOPIC,
-        REPLAN_REASON_TOPIC,
-        CURRENT_FRAME_TOPIC,
-    ]
+#tf_transformer = tf_bag.BagTfTransformer(bag)
+tf_transformer = None
+reasoner = StopReasoner(tf_transformer)
 
-    stop_reason_filter = StopReasonFilter(["NO_NAVIGATION", "NOT_STOPPED", "NO_TOUCH", "STOPPED_BUT_UNDER_THRESHOLD"])
+prev_code = None
+prev_duration = 0
 
-    storage_filter = rosbag2_py.StorageFilter(topics=all_topics)
-    reader.set_filter(storage_filter)
-    
-    while reader.has_next():
-        (topic, msg_data, t) = reader.read_next()
-        msg_type = get_message(type_map[topic])
-        msg = deserialize_message(msg_data, msg_type)
+ODOM_TOPIC = "/cabot/odom_raw"
+EVENT_TOPIC = "/cabot/event"
+CMD_VEL_TOPIC = "/cmd_vel"
+PEOPLE_SPEED_TOPIC = "/cabot/people_speed"
+TF_SPEED_TOPIC = "/cabot/tf_speed"
+TOUCH_SPEED_TOPIC = "/cabot/touch_speed_switched"
+RECEIVED_GLOBAL_PLAN = "/received_global_plan"
+REPLAN_REASON_TOPIC = "/replan_reason"
+CURRENT_FRAME_TOPIC = "/current_frame"
+
+all_topics = [
+    ODOM_TOPIC,
+    EVENT_TOPIC,
+    CMD_VEL_TOPIC,
+    PEOPLE_SPEED_TOPIC,
+    TF_SPEED_TOPIC,
+    TOUCH_SPEED_TOPIC,
+    RECEIVED_GLOBAL_PLAN,
+    REPLAN_REASON_TOPIC,
+    CURRENT_FRAME_TOPIC,
+]
+
+stop_reason_filter = StopReasonFilter(["NO_NAVIGATION", "NOT_STOPPED", "NO_TOUCH", "STOPPED_BUT_UNDER_THRESHOLD"])
+#stop_reason_filter = StopReasonFilter([])
+
+reader.set_filter_by_topics(all_topics)
+reader.set_filter_by_options(options)  # filter by start and duration
+
+prev_code = None
+
+rclpy.init()
+
+while reader.has_next() and rclpy.ok():
+    try:
+        (topic, msg, t, st) = reader.serialize_next()
+        if t is None:
+            continue
+        t = t*1e9
         reasoner.update_time(t)
 
         code = None
@@ -85,10 +94,8 @@ def read_from_bag(bagfile, callback=None):
             reasoner.input_odom(msg)
         elif topic == EVENT_TOPIC:
             reasoner.input_event(msg)
-        elif topic in [NAVIGATE_TO_POSE_GOAL_TOPIC, LOCAL_PREFIX+NAVIGATE_TO_POSE_GOAL_TOPIC]:
-            reasoner.input_goal_topic(msg)
-        elif topic in [NAVIGATE_TO_POSE_RESULT_TOPIC, LOCAL_PREFIX+NAVIGATE_TO_POSE_RESULT_TOPIC]:
-            reasoner.input_result_topic(msg)
+        elif topic == RECEIVED_GLOBAL_PLAN:
+            reasoner.input_global_plan(msg)
         elif topic == CMD_VEL_TOPIC:
             reasoner.input_cmd_vel(msg)
         elif topic == PEOPLE_SPEED_TOPIC:
@@ -103,12 +110,17 @@ def read_from_bag(bagfile, callback=None):
             reasoner.input_current_frame(msg)
 
         (duration, code) = reasoner.update()
+        if prev_code != code:
+            logger.debug("%.2f(%.2f), %s, %.2f", t/1e9, st, code.name, duration)
+        prev_code = code
         stop_reason_filter.update(duration, code)
         (duration, code) = stop_reason_filter.summary()
         stop_reason_filter.conclude()
 
         if code:
-            logger.info("%.2f, %s, %.2f", t/1e9, code.name, duration)
+            logger.info("### %.2f(%.2f), %s, %.2f, %s", t/1e9, st, code.name, duration,
+                        "NAVIGATING" if reasoner.navigating else "NOT NAVIGATING")
+    except:
+        import traceback
+        logger.error(traceback.format_exc())
 
-if __name__ == "__main__":
-    read_from_bag(sys.argv[1])
