f40f910d2032011c9a37b45df28e7624ffece08e
Fuki Miyazaki <f2miyazaki@lab.miraikan.jst.go.jp>
2023-11-07 17:46:51 +0900

    fix: use constant
    Signed-off-by: Fuki Miyazaki <f2miyazaki@lab.miraikan.jst.go.jp>

--- cabot_ui/scripts/navcog_map.py
+++ cabot_ui/scripts/navcog_map.py
@@ -44,7 +44,7 @@ last_floor = None
 menu_handler = MenuHandler()
 server = None
 map_frame = "map_global"
-localize_status = 0
+localize_status = MFLocalizeStatus.UNKNOWN
 
 
 def visualize_features(features, node_map):
