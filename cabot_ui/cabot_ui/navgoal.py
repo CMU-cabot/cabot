94896136f4153a1ec0b72a53311d68e48c1ef41a
tatsuya-ishihara <tisihara@jp.ibm.com>
2023-12-01 14:31:30 +0900

    fix narrow bug
    Signed-off-by: tatsuya-ishihara <tisihara@jp.ibm.com>

--- cabot_ui/cabot_ui/navgoal.py
+++ cabot_ui/cabot_ui/navgoal.py
@@ -741,6 +741,10 @@ class NarrowGoal(NavGoal):
         if 'need_to_announce_follow' in kwargs:
             self._need_to_announce_follow = bool(kwargs['need_to_announce_follow'])
 
+        self._is_last = False
+        if 'is_last' in kwargs:
+            self._is_last = bool(kwargs['is_last'])
+
         self._tag = None
         facility = None
         if isinstance(navcog_route[0], geojson.RouteLink):
