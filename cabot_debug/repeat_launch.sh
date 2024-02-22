0020f1224e97e6a0e78b74b1a24e568b21809400
Fuki Miyazaki <f2miyazaki@lab.miraikan.jst.go.jp>
2024-02-22 14:36:20 +0900

    fix repeat_launch.sh and add set-floor_and_estimate.sh
    Signed-off-by: Fuki Miyazaki <f2miyazaki@lab.miraikan.jst.go.jp>

--- cabot_debug/repeat_launch.sh
+++ cabot_debug/repeat_launch.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-trap 'systemctl --user stop cabot.service; exit' SIGINT
+stop_launch() {
+    kill $launch_pid
+    wait $launch_pid
+    exit 0
+}
 
+trap 'stop_launch' SIGINT SIGTERM EXIT
 
 function help()
 {
@@ -14,9 +19,10 @@ function help()
 }
 
 floor=0
-repeat_times=20
+repeat_times=2
 usb_recognition=0
 no_repeat=0
+wait_map=0
 
 CABOT_INITX=0
 CABOT_INITY=0
@@ -28,6 +34,9 @@ scriptdir=`pwd`
 
 cd $scriptdir/../
 source ./.env
+
+projectdir=`pwd`
+project=$(basename $projectdir)
 
 while getopts "hf:t:un" arg; do
     case $arg in
@@ -61,23 +70,30 @@ pause(){
     wait $!
 }
 
-
 for i in `eval echo {1..$repeat_times}`; do
-    systemctl --user start cabot.service
+    if [[ -z $(docker ps -q -f "name=${project}-map_server*") ]]; then
+        wait_map=1
+    fi
+
+    ./launch.sh -y &
+    launch_pid=$!
+
     sleep 10
-    if [[ $i -eq 1 ]]; then
+
+    if [[ $wait_map -eq 1 ]]; then
 	echo "wait for map server to start"
 	sleep 20
     fi
-    container_id=`docker ps | grep localization | cut -d " " -f 1`
-    docker exec $container_id bash -c 'source install/setup.bash; ros2 service call /set_current_floor mf_localization_msgs/srv/MFSetInt "{data: '$floor'}"'
+
+    ./cabot_debug/set-floor_and_estimate.sh -f $floor
+    
     sleep 10
-    ros2 topic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped '{ header: {frame_id: "map_global"}, pose: { pose: {position: {x: '$CABOT_INITX', y: '$CABOT_INITY'}}}}'
-    sleep 180
+
     if [[ $no_repeat -eq 1 ]]; then
 	pause
     fi
-    systemctl --user stop cabot.service
+    kill $launch_pid
+    wait $launch_pid
     if [[ $CABOT_MODEL == "cabot2-gtm" ]] && [[ $usb_recognition -eq 1 ]]; then
         count=`lsusb | grep 435 | wc -l`
         if [[ $count -le 1 ]]; then
---
+++ cabot_debug/set-floor_and_estimate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function help()
+{
+    echo "Usage:"
+    echo "-h          show this help"
+    echo "-f          set floor"
+}
+
+
+floor=0
+CABOT_INITX=0
+CABOT_INITY=0
+
+pwd=`pwd`
+scriptdir=`dirname $0`
+cd $scriptdir
+scriptdir=`pwd`
+
+cd $scriptdir/../
+source ./.env
+
+while getopts "hf:" arg; do
+    case $arg in
+        h)
+            help
+            exit
+            ;;
+        f)
+            floor=$OPTARG
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+container_id=`docker ps | grep localization | cut -d " " -f 1`
+docker exec $container_id bash -c 'source install/setup.bash; ros2 service call /set_current_floor mf_localization_msgs/srv/MFSetInt "{data: '$floor'}"'
+sleep 3
+ros2 topic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped '{ header: {frame_id: "map_global"}, pose: { pose: {position: {x: '$CABOT_INITX', y: '$CABOT_INITY'}}}}'
+
