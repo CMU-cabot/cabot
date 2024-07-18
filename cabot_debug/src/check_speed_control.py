3d1029bfce62bf199a8e13f664958af9cea5120d
fukimiyazaki <f2miyazaki@lab.miraikan.jst.go.jp>
2024-07-18 17:22:11 +0900

    add angular
    Signed-off-by: fukimiyazaki <f2miyazaki@lab.miraikan.jst.go.jp>

--- cabot_debug/src/check_speed_control.py
+++ cabot_debug/src/check_speed_control.py
@@ -80,9 +80,10 @@ while reader.has_next():
     if topic in [ 
             "/cabot/cmd_vel",
             "/cmd_vel"]:
-        i = getIndex(topic, 2)
+        i = getIndex(topic, 3)
         data[i].append(st)
         data[i+1].append(msg.linear.x)
+        data[i+2].append(msg.angular.z)
     elif topic in [
             "/cabot/touch",
             "/cabot/touch_raw",
@@ -113,14 +114,16 @@ touch_frame.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
 
 # Create a Matlotlib figure
 fig, ax1 = plt.subplots(figsize=(20, 10))
-line1, = ax1.plot([], [], 'red', linestyle='-', label='/cabot/cmd_vel')
+line1, = ax1.plot([], [], 'red', linestyle='-', label='/cabot/cmd_vel.l')
 line2, = ax1.plot([], [], 'blue', linestyle='--', label='/cabot/touch')
 line3, = ax1.plot([], [], 'green', linestyle=':', label='/cabot/lidar_speed')
 line4, = ax1.plot([], [], 'orange', linestyle=':', label='/cabot/people_speed')
 line6, = ax1.plot([], [], 'brown', linestyle='-', label='/cabot/tf_speed')
 line7, = ax1.plot([], [], 'pink', linestyle='-', label='/cabot/map_speed')
 line8, = ax1.plot([], [], 'cyan', linestyle='-', label='/cabot/user_speed')
-line9, = ax1.plot([], [], 'yellow', linestyle='-', label='/cmd_vel')
+line9, = ax1.plot([], [], 'yellow', linestyle='-', label='/cmd_vel.l')
+line10, = ax1.plot([], [], 'teal', linestyle='-', label='/cabot/cmd_vel.r')
+line11, = ax1.plot([], [], 'magenta', linestyle='-', label='/cmd_vel.r')
 ax1.legend(bbox_to_anchor=(1.00, 1), loc='upper left')
 
 ax2 = ax1.twinx()
@@ -133,6 +136,8 @@ line6.set_visible(False)
 line7.set_visible(False)
 line8.set_visible(False)
 line9.set_visible(False)
+line10.set_visible(False)
+line11.set_visible(False)
 
 # Embed the Matplotlib figure into the Tkinter window using FigureCanvasTkAgg
 canvas = FigureCanvasTkAgg(fig, master=root)
@@ -164,6 +169,8 @@ def plot_data():
     line7.set_data(data[getIndex("/cabot/map_speed")], data[getIndex("/cabot/map_speed")+1])
     line8.set_data(data[getIndex("/cabot/user_speed")], data[getIndex("/cabot/user_speed")+1])
     line9.set_data(data[getIndex("/cmd_vel")], data[getIndex("/cmd_vel")+1])
+    line10.set_data(data[getIndex("/cabot/cmd_vel")], data[getIndex("/cabot/cmd_vel")+2])
+    line11.set_data(data[getIndex("/cmd_vel")], data[getIndex("/cmd_vel")+2])
     ax1.relim()
     ax1.autoscale_view()
     ax2.relim()
@@ -186,7 +193,9 @@ var6 = tk.BooleanVar(value=False)
 var7 = tk.BooleanVar(value=False)
 var8 = tk.BooleanVar(value=False)
 var9 = tk.BooleanVar(value=False)
-checkbox1 = tk.Checkbutton(cmd_vel_frame, text="Show /cabot/cmd_vel", variable=var1, command=lambda: toggle_line(line1, var1, ax1))
+var10 = tk.BooleanVar(value=False)
+var11 = tk.BooleanVar(value=False)
+checkbox1 = tk.Checkbutton(cmd_vel_frame, text="Show /cabot/cmd_vel.l", variable=var1, command=lambda: toggle_line(line1, var1, ax1))
 checkbox2 = tk.Checkbutton(touch_frame, text="Show /cabot/touch", variable=var2, command=lambda: toggle_line(line2, var2, ax1))
 checkbox3 = tk.Checkbutton(speed_frame, text="Show /cabot/lidar_speed", variable=var3, command=lambda: toggle_line(line3, var3, ax1))
 checkbox4 = tk.Checkbutton(speed_frame, text="Show /cabot/people_speed", variable=var4, command=lambda: toggle_line(line4, var4, ax1))
@@ -194,14 +203,16 @@ checkbox5 = tk.Checkbutton(touch_frame, text="Show /cabot/touch_raw", variable=v
 checkbox6 = tk.Checkbutton(speed_frame, text="Show /cabot/tf_speed", variable=var6, command=lambda: toggle_line(line6, var6, ax1))
 checkbox7 = tk.Checkbutton(speed_frame, text="Show /cabot/map_speed", variable=var7, command=lambda: toggle_line(line7, var7, ax1))
 checkbox8 = tk.Checkbutton(speed_frame, text="Show /cabot/user_speed", variable=var8, command=lambda: toggle_line(line8, var8, ax1))
-checkbox9 = tk.Checkbutton(cmd_vel_frame, text="Show /cmd_vel", variable=var9, command=lambda: toggle_line(line9, var9, ax1))
+checkbox9 = tk.Checkbutton(cmd_vel_frame, text="Show /cmd_vel.l", variable=var9, command=lambda: toggle_line(line9, var9, ax1))
+checkbox10 = tk.Checkbutton(cmd_vel_frame, text="Show /cabot/cmd_vel.r", variable=var10, command=lambda: toggle_line(line10, var10, ax1))
+checkbox11 = tk.Checkbutton(cmd_vel_frame, text="Show /cmd_vel.r", variable=var11, command=lambda: toggle_line(line11, var11, ax1))
 
 # Create category checkboxes
 cmd_vel_var = tk.BooleanVar(value=True)
 speed_var = tk.BooleanVar(value=True)
 touch_var = tk.BooleanVar(value=True)
 
-cmd_vel_checkbox = tk.Checkbutton(cmd_vel_frame, text="all", variable=cmd_vel_var, command=lambda: toggle_category(cmd_vel_var, [(var1, line1), (var9, line9)]))
+cmd_vel_checkbox = tk.Checkbutton(cmd_vel_frame, text="all", variable=cmd_vel_var, command=lambda: toggle_category(cmd_vel_var, [(var1, line1), (var9, line9), (var10, line10), (var11, line11)]))
 speed_checkbox = tk.Checkbutton(speed_frame, text="all", variable=speed_var, command=lambda: toggle_category(speed_var, [(var3, line3), (var4, line4), (var6, line6), (var7, line7), (var8, line8)]))
 touch_checkbox = tk.Checkbutton(touch_frame, text="all", variable=touch_var, command=lambda: toggle_category(touch_var, [(var2, line2), (var5, line5)]))
 
@@ -209,6 +220,8 @@ touch_checkbox = tk.Checkbutton(touch_frame, text="all", variable=touch_var, com
 cmd_vel_checkbox.pack(side=tk.TOP, anchor='w')
 checkbox1.pack(side=tk.TOP, anchor='w')
 checkbox9.pack(side=tk.TOP, anchor='w')
+checkbox10.pack(side=tk.TOP, anchor='w')
+checkbox11.pack(side=tk.TOP, anchor='w')
 speed_checkbox.pack(side=tk.TOP, anchor='w')
 checkbox3.pack(side=tk.TOP, anchor='w')
 checkbox4.pack(side=tk.TOP, anchor='w')
