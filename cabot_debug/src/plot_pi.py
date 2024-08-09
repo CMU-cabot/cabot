43f05e38212a6016ed8438aec55c1eddaccc153e
yamahatayukiya <y2yamahata@jst.go.jp>
2024-08-09 18:01:26 +0900

    fix : highlight function for plot_pi
    Signed-off-by: yamahatayukiya <y2yamahata@jst.go.jp>

--- cabot_debug/src/plot_pi.py
+++ cabot_debug/src/plot_pi.py
@@ -27,6 +27,7 @@ from optparse import OptionParser
 import matplotlib.pyplot as plt
 from cabot_common.rosbag2 import BagReader
 from matplotlib.widgets import CheckButtons
+import numpy as np
 
 parser = OptionParser(usage="""
 Example:
@@ -36,6 +37,8 @@ Example:
 parser.add_option('-f', '--file', type=str, help='bag file to be processed')
 parser.add_option('-s', '--start', type=float, help='start time from the begining', default=0.0)
 parser.add_option('-d', '--duration', type=float, help='duration from the start time', default=99999999999999)
+parser.add_option('-t', '--threshold', type=float, help='threshold value fot the integral', default=0.2)
+parser.add_option('-p', '--period', type=float, help='time period to check for threshold exceedance', default=10.0)
 
 (options, args) = parser.parse_args()
 
@@ -44,6 +47,8 @@ if not options.file:
     sys.exit(0)
 
 bagfilename = options.file
+threshold = options.threshold
+period = options.period
 reader = BagReader(bagfilename)
 
 reader.set_filter_by_topics(["/PI_control",])
@@ -60,48 +65,82 @@ control_errors = {
     "error_spd_turn": [],
     "integral_turn": []
 }
+try:
+    while reader.has_next():
+        (topic, msg, t, st) = reader.serialize_next()
+        if msg is None:
+            continue
+        try:
+            time_stamps.append(st)
+            control_errors["current_spd_linear"].append(msg.current_spd_linear)
+            control_errors["measured_spd_linear"].append(msg.measured_spd_linear)
+            control_errors["error_spd_linear"].append(msg.error_spd_linear)
+            control_errors["integral_linear"].append(msg.integral_linear)
+            control_errors["target_spd_turn"].append(msg.target_spd_turn)
+            control_errors["measured_spd_turn"].append(msg.measured_spd_turn)
+            control_errors["error_spd_turn"].append(msg.error_spd_turn)
+            control_errors["integral_turn"].append(msg.integral_turn)
+        except Exception as e:
+            print(f"Failed to deserialize msg: {e}")
+            time_stamps.pop()
 
-while reader.has_next():
-    (topic, msg, t, st) = reader.serialize_next()
-    try:
-        time_stamps.append(st)
-        control_errors["current_spd_linear"].append(msg.current_spd_linear)
-        control_errors["measured_spd_linear"].append(msg.measured_spd_linear)
-        control_errors["error_spd_linear"].append(msg.error_spd_linear)
-        control_errors["integral_linear"].append(msg.integral_linear)
-        control_errors["target_spd_turn"].append(msg.target_spd_turn)
-        control_errors["measured_spd_turn"].append(msg.measured_spd_turn)
-        control_errors["error_spd_turn"].append(msg.error_spd_turn)
-        control_errors["integral_turn"].append(msg.integral_turn)
-    except Exception as e:
-        print(f"Failed to deserialize msg: {e}")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    lines = {
+        "current_spd_linear": ax.plot(time_stamps, control_errors["current_spd_linear"], label='Current Speed Linear')[0],
+        "measured_spd_linear": ax.plot(time_stamps, control_errors["measured_spd_linear"], label='Measured Speed Linear')[0],
+        "error_spd_linear": ax.plot(time_stamps, control_errors["error_spd_linear"], label='Error Speed Linear')[0],
+        "integral_linear": ax.plot(time_stamps, control_errors["integral_linear"], label='Integral Linear')[0],
+        "target_spd_turn": ax.plot(time_stamps, control_errors["target_spd_turn"], label='Target Speed Turn')[0],
+        "measured_spd_turn": ax.plot(time_stamps, control_errors["measured_spd_turn"], label='Measured Speed Turn')[0],
+        "error_spd_turn": ax.plot(time_stamps, control_errors["error_spd_turn"], label='Error Speed Turn')[0],
+        "integral_turn": ax.plot(time_stamps, control_errors["integral_turn"], label='Integral Turn')[0]
+    }
 
-fig, ax = plt.subplots(figsize=(10, 6))
-lines = {
-    "current_spd_linear": ax.plot(time_stamps, control_errors["current_spd_linear"], label='Current Speed Linear')[0],
-    "measured_spd_linear": ax.plot(time_stamps, control_errors["measured_spd_linear"], label='Measured Speed Linear')[0],
-    "error_spd_linear": ax.plot(time_stamps, control_errors["error_spd_linear"], label='Error Speed Linear')[0],
-    "integral_linear": ax.plot(time_stamps, control_errors["integral_linear"], label='Integral Linear')[0],
-    "target_spd_turn": ax.plot(time_stamps, control_errors["target_spd_turn"], label='Target Speed Turn')[0],
-    "measured_spd_turn": ax.plot(time_stamps, control_errors["measured_spd_turn"], label='Measured Speed Turn')[0],
-    "error_spd_turn": ax.plot(time_stamps, control_errors["error_spd_turn"], label='Error Speed Turn')[0],
-    "integral_turn": ax.plot(time_stamps, control_errors["integral_turn"], label='Integral Turn')[0]
-}
+    for key in ["integral_turn"]:
+        integral = np.array(control_errors[key])
+        timestamps = np.array(time_stamps)
+        exceed_indices = np.where(integral > threshold)[0]
+        if threshold < 0:
+            exceed_indices = np.where(integral < threshold)[0]
+        if len(exceed_indices) > 0:
+            for start_idx in exceed_indices:
+                end_idx = start_idx
+                while (end_idx < len(integral) and timestamps[end_idx] - timestamps[start_idx] <= period and integral[end_idx] > threshold):
+                    end_idx += 1
+                if timestamps[end_idx-1] - timestamps[start_idx] >= period:
+                    ax.axvspan(timestamps[start_idx], timestamps[end_idx-1], color='aquamarine', alpha=0.5, label=f'{key} exceeds threshold')
 
-plt.xlabel('Time (s)')
-plt.ylabel('Control Error value')
-plt.title('PI Control Data over Time')
-plt.grid(True)
+    for key in ["integral_linear"]:
+        integral = np.array(control_errors[key])
+        timestamps = np.array(time_stamps)
+        exceed_indices = np.where(integral > threshold)[0]
+        if threshold < 0:
+            exceed_indices = np.where(integral < threshold)[0]
+        if len(exceed_indices) > 0:
+            for start_idx in exceed_indices:
+                end_idx = start_idx
+                while (end_idx < len(integral) and timestamps[end_idx] - timestamps[start_idx] <= period and integral[end_idx] > threshold):
+                    end_idx += 1
+                if timestamps[end_idx-1] - timestamps[start_idx] >= period:
+                    ax.axvspan(timestamps[start_idx], timestamps[end_idx-1], color='tomato', alpha=0.5, label=f'{key} exceeds threshold')
 
-rax = plt.axes([0.9, 0.4, 0.15, 0.4], facecolor='lightgoldenrodyellow')
-labels = list(lines.keys())
-visibility = [line.get_visible() for line in lines.values()]
-check = CheckButtons(rax, labels, visibility)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Control Error value')
+    plt.title('PI Control Data over Time')
+    plt.grid(True)
 
-def func(label):
-    line = lines[label]
-    line.set_visible(not line.get_visible())
-    plt.draw()
+    rax = plt.axes([0.9, 0.4, 0.15, 0.4], facecolor='lightgoldenrodyellow')
+    labels = list(lines.keys())
+    visibility = [line.get_visible() for line in lines.values()]
+    check = CheckButtons(rax, labels, visibility)
 
-check.on_clicked(func)
-plt.show()
+    def func(label):
+        line = lines[label]
+        line.set_visible(not line.get_visible())
+        plt.draw()
+
+    check.on_clicked(func)
+    plt.show()
+
+except KeyboardInterrupt:
+    print("Script interrupted by user.")
