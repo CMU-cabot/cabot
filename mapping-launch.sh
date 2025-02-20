dccf68886119770e64d56d5033d6b82d6a9799f5
Masayuki Murata <muratams@jp.ibm.com>
2025-02-20 14:24:01 +0900

    fix mapping-launch.sh script to export only variables starting with CYCLONEDDS
    Signed-off-by: Masayuki Murata <muratams@jp.ibm.com>

--- mapping-launch.sh
+++ mapping-launch.sh
@@ -152,9 +152,10 @@ pwd=`pwd`
 scriptdir=`dirname $0`
 cd $scriptdir
 scriptdir=`pwd`
-source $scriptdir/.env
 
-## if network interface name for Cyclone DDS is not specified, set autoselect as true
+# export variables starting with CYCLONEDDS_ from .env file
+export $(grep -E '^CYCLONEDDS_' $scriptdir/.env | xargs)
+# if network interface name for Cyclone DDS is not specified, set autoselect as true
 if [ ! -z $CYCLONEDDS_URI ]; then
     if [ ! -z $CYCLONEDDS_NETWORK_INTERFACE_NAME ]; then
         export CYCLONEDDS_NETWORK_INTERFACE_AUTODETERMINE="false"
