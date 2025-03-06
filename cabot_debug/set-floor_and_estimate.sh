ef71550b3321073cdb4867b100ee9995caecdfea
Masayuki Murata <muratams@jp.ibm.com>
2025-03-06 15:15:22 +0900

    fix set-floor_and_estimate.sh to work correctly when localization image is shared by two or more containers
    Signed-off-by: Masayuki Murata <muratams@jp.ibm.com>

--- cabot_debug/set-floor_and_estimate.sh
+++ cabot_debug/set-floor_and_estimate.sh
@@ -34,7 +34,7 @@ done
 shift $((OPTIND-1))
 
 
-container_id=`docker ps | grep localization | cut -d " " -f 1`
+container_id=`docker ps --format "{{.ID}} {{.Names}}" | grep localization | cut -d " " -f 1`
 docker exec $container_id bash -c 'source install/setup.bash; ros2 service call /set_current_floor mf_localization_msgs/srv/MFSetInt "{data: '$floor'}"'
 sleep 3
 ros2 topic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped '{ header: {frame_id: "map_global"}, pose: { pose: {position: {x: '$CABOT_INITX', y: '$CABOT_INITY'}}}}'
