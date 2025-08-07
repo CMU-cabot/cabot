38ef34f55901f1e659bc2783e75d406e352b3266
Daisuke Sato <daisukes@cmu.edu>
2025-08-07 10:13:22 -0400

    improve workspace build
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- build-workspace.sh
+++ build-workspace.sh
@@ -48,7 +48,7 @@ build_docker_ws=0
 build_host_ws=0
 dcfiles=("docker-compose.yaml")
 
-while getopts "hdswop" arg; do
+while getopts "hdswo" arg; do
     case $arg in
     h)
         help
@@ -66,9 +66,6 @@ while getopts "hdswop" arg; do
     o)
         build_host_ws=1
         ;;
-    p)
-	dcfiles=("docker-compose-phone.yaml")
-	;;
     esac
 done
 shift $((OPTIND-1))
